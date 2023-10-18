316e601280d118cdf288684e736164d81613f8e0
Scarlett0JS <scarlett.js1010@gmail.com>
2023-10-18 23:34:40 +0900

    Delete OG Folder

--- OG Default Project/FinalDefault/src/main/java/com/theme/frontcontroller/ViewResolver.java
+++
@@ -1,7 +0,0 @@
-package com.theme.frontcontroller;
-
-public class ViewResolver {
-	public static String makeView(String nextPage) {
-		return "WEB-INF/FinalView/" + nextPage + ".jsp"; 
-	}
-}
--- OG Default Project/Html/test.js
+++
@@ -1,2 +0,0 @@
-console.log("*".repeat("11".length))
-
